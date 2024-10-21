a99c8761e18e803eba78a83a234844a3fcedb39f
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-10-21 20:10:32 +0900

    Updated mentors animation

--- components/mentor-card/MentorCard.jsx
+++ components/mentor-card/MentorCard.jsx
@@ -1,23 +1,36 @@
+"use client";
+
 import React from "react";
 import styles from "./mentor-card.module.css";
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { joinClassNames } from "@/utils/join-class-names";
 
-const MentorCard = ({ photo, name, position, caption, href }) => (
-  <a className={styles.container} href={href} target="_blank">
-    <div className={styles.photo}>
-      <Image
-        src={photo}
-        alt={name}
-        fill
-        className={styles.cover}
-      />
-    </div>
-    <div className={styles.texts}>
-      <div>{name}</div>
-      <div className={styles.jobname}>{position}</div>
-    </div>
-    {/* <div className={styles.caption}>{caption}</div> */}
-  </a>
-);
+const MentorCard = ({ photo, name, position, caption, href }) => {
+  const { ref, inView } = useInView({ triggerOnce: true });
+
+  return (
+    <a
+      className={joinClassNames(styles.container, !inView ? styles.hidden : undefined)}
+      ref={ref}
+      href={href}
+      target="_blank"
+    >
+      <div className={styles.photo}>
+        <Image
+          src={photo}
+          alt={name}
+          fill
+          className={styles.cover}
+        />
+      </div>
+      <div className={styles.texts}>
+        <div>{name}</div>
+        <div className={styles.jobname}>{position}</div>
+      </div>
+      {/* <div className={styles.caption}>{caption}</div> */}
+    </a>
+  );
+};
 
 export default MentorCard;
--- components/mentors/Mentors.jsx
+++ components/mentors/Mentors.jsx
@@ -1,17 +1,11 @@
-"use client";
-
 import React from "react";
 import styles from "./mentors.module.css";
 import { mentors } from "@/data";
 import MentorCard from "@/components/mentor-card/MentorCard";
-import { useInView } from "react-intersection-observer";
-import { joinClassNames } from "@/utils/join-class-names";
 
 const Mentors = () => {
-  const { ref, inView, entry } = useInView({ triggerOnce: true });
-
   return (
-    <div className={joinClassNames(styles.mentors, !inView ? styles.hidden : undefined)} ref={ref}>
+    <div className={styles.mentors}>
       {mentors.map((mentor) => (
         <MentorCard key={mentor.name} {...mentor} />
       ))}
